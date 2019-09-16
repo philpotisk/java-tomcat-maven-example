384fcf5fade5eb6bb60cdc58fc4e5e91c75a7319
Philipp Potisk <philipp.potisk@danubetech.com>
2019-09-16 14:48:48 +0200

    Added docker-push action

---
+++ actions/docker-push/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -l
+
+sh -c "echo DOCKER BUILD AND PUSH"
+
+sudo docker build . -t phil21/java-hello-world
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+docker push phil21/java-hello-world:latest
