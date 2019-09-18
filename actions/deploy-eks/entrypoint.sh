145bcd268fab2046f8a56205fcb5d58845036b77
Philipp Potisk <philipp.potisk@danubetech.com>
2019-09-18 16:39:28 +0200

    Refactored Dockerfile

--- actions/deploy-eks/entrypoint.sh
+++ actions/deploy-eks/entrypoint.sh
@@ -9,3 +9,6 @@ echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
 sh -c "kubectl $*"
+
+#sh -c "kubectl version"
+#sh -c "kubectl apply -f ../../deployment-java-hello-world.yaml "
